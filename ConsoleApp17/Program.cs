1614a7f4987df460679353fb600bad9645c3afd8
yevhen_luchko@epam.com <yevhen_luchko@epam.com>
2025-02-07 12:42:04 +0200

    qwer

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp17
+{
+    public class Program
+    {
+        protected Program()
+            {
+                    
+            }
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.F(q =>
+            {
+                q.Q1();
+                q.Q();
+            });
+        }
+    }
+
+
+    class MyClass
+        {
+        B b=new B();
+
+       public void F(Action<B> action) {
+            action.Invoke(b);
+        }
+
+        }
+
+    internal class B
+        {
+        public void Q() { }
+        public void Q1() { }
+        }
+    }
